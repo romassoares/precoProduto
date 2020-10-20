7d2a73272818b505bdcb1542664971f5746458a4
LuizPedroSC <33464368+LuizPedroSC@users.noreply.github.com>
2020-10-20 19:42:33 -0300

    Update ClientController.php

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -17,7 +17,7 @@ class ClientController extends Controller
 
     public function index()
     {
-        $clients = $this->obj->all();
+        $clients = $this->obj->paginate(15);
         return view('system/Client/index', ['clients' => $clients]);
     }
 
